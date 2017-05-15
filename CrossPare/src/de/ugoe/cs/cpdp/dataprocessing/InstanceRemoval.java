6f64dd636daf1b47cb4f4b779995e4667e0990d5
atx <atx@drecks-provider.de>
2017-05-15 17:10:47 +0200

    add InstanceRemoval dataselection, can be used to exclude instances from training and test data based on values of their respective attributes

---
+++ CrossPare/src/de/ugoe/cs/cpdp/dataprocessing/InstanceRemoval.java
@@ -0,0 +1,70 @@
+// Copyright 2015 Georg-August-Universität Göttingen, Germany
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package de.ugoe.cs.cpdp.dataprocessing;
+
+import java.util.Arrays;
+
+import org.apache.commons.collections4.list.SetUniqueList;
+import weka.core.Instances;
+
+/**
+ * <p>
+ * Remove instances from training and testdata by attributes and their values.
+ * 
+ * Examle:
+ * <setwisepreprocessor name="InstanceRemoval" param="test1:0,test2:1" />
+ * 
+ * Removes every instance where attribute test1 is 0 and test2 is 1 (value are parsed to Double).
+ * </p>
+ * 
+ * @author Alexander Trautsch
+ */
+public class InstanceRemoval implements ISetWiseProcessingStrategy, IProcessesingStrategy {
+    private String[] condList;  // holds an array of conditions, every condition needs to match to remove an instance.
+    
+    @Override
+    public void setParameter(String parameters) {
+        condList = parameters.split(",");        
+    }
+
+    @Override
+    public void apply(Instances testdata, Instances traindata) {
+        removeInstances(testdata);
+        removeInstances(traindata);
+    }
+    
+    private void removeInstances(Instances ilist) {
+        for (int i = 0; i < ilist.numInstances(); i++) {
+            Boolean matchFound[] = new Boolean[condList.length];
+            int j = 0;
+            for(String cond : condList) {
+                String[] match = cond.split(":");
+                matchFound[j] = ilist.get(i).value(ilist.attribute(match[0])) == Double.parseDouble(match[1]);
+            }
+            // remove Instance only if every condition is true
+            if (!Arrays.asList(matchFound).contains(false)) {
+                ilist.delete(i);
+            }
+        }
+    }
+
+    @Override
+    public void apply(Instances testdata, SetUniqueList<Instances> traindataSet) {
+        removeInstances(testdata);
+        for (Instances traindata : traindataSet) {
+            removeInstances(traindata);
+        }
+    }
+}
---
+++ CrossPare/test/de/ugoe/cs/cpdp/dataprocessing/InstanceRemovalTest.java
@@ -0,0 +1,52 @@
+package de.ugoe.cs.cpdp.dataprocessing;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import de.ugoe.cs.cpdp.dataprocessing.InstanceRemoval;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instances;
+
+public class InstanceRemovalTest {
+
+    @Test
+    public void testSetParameter() {
+        new InstanceRemoval().setParameter("test=0,another=1");
+    }
+    
+    @Test
+    public void testApply() {
+        ArrayList<Attribute> attributes = new ArrayList<Attribute>();
+        attributes.add(new Attribute("test"));
+        attributes.add(new Attribute("another"));
+        attributes.add(new Attribute("class"));
+        
+        Instances testdata = new Instances("testdata", attributes, 0);
+        testdata.setClassIndex(2);
+        testdata.add(new DenseInstance(1.0, new double[]{1.0, 0.0, 0.0}));
+        testdata.add(new DenseInstance(1.0, new double[]{0.0, 0.0, 0.0}));
+        testdata.add(new DenseInstance(1.0, new double[]{0.0, 1.0, 0.0}));
+                
+        Instances traindata = new Instances("traindata", attributes, 0);
+        traindata.setClassIndex(2);
+        traindata.add(new DenseInstance(1.0, new double[]{1.0, 0.0, 0.0}));
+        traindata.add(new DenseInstance(1.0, new double[]{0.0, 0.0, 0.0}));
+        traindata.add(new DenseInstance(1.0, new double[]{0.0, 1.0, 0.0}));
+        
+        InstanceRemoval filter = new InstanceRemoval();
+        filter.setParameter("test=0,another=1");
+        filter.apply(testdata, traindata);
+        
+        assertTrue(traindata.numInstances() == 1);
+        assertTrue(traindata.get(0).value(traindata.attribute("test")) == 0.0);
+        assertTrue(traindata.get(0).value(traindata.attribute("another")) == 1.0);
+        
+        assertTrue(testdata.numInstances() == 1);
+        assertTrue(testdata.get(0).value(testdata.attribute("test")) == 0.0);
+        assertTrue(testdata.get(0).value(testdata.attribute("another")) == 1.0);
+    }
+}
